3ac72a62ffc6cb0951f94c5f44f666ff0a73a521
Immolazione <federico.deered@gmail.com>
2022-02-14 12:42:07 +0100

    start building base structure

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+<header></header>
+</template>
+
+<script>
+export default {
+name: 'Header.vue',
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,13 @@
+<template>
+<main></main>
+</template>
+
+<script>
+export default {
+name: 'Main.vue'
+}
+</script>
+
+<style lang="scss">
+
+</style>
