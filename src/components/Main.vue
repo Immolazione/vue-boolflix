d2a2abbdf0d5d04e5ba8447555c5aa0812390c64
Immolazione <federico.deered@gmail.com>
2022-02-14 17:36:06 +0100

    add functioning searchBar on console

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,20 +2,37 @@
 <main>
     <div>
     <input type="text" v-model="searchText">
-    <button type="button">Search</button>
+    <button type="button" @click="getFilms">Search</button>
     </div>
 </main>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    name: 'Main',
-    data(){
-        return {
-            searchText: "",
-        }
-    }
-}
+  name: 'Main',
+  data() {
+    return {
+      searchText: "",
+      filmsFound: [],
+      baseUri: 'https://api.themoviedb.org/3',
+      apiKey: '0d07b420b3a9f7101c92e8e8e3e0ee15',
+      endPoint: '/search/movie',
+      language: 'it-IT',
+      queryString: '',
+    };
+  },
+  
+  methods: {
+    getFilms(){
+      axios.get(`${this.baseUri}/search/movie?api_key=${this.apiKey}&language=it-IT&query=${this.searchText}`).then(res => {
+        console.log(res.data.results);
+        this.filmsFound = res.data.results;
+      });
+    },
+  },
+};  
 </script>
 
 <style lang="scss">
